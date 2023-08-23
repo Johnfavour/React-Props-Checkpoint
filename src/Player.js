efcf4a3d239f139cbd77df315424b2bf95c6fa47
Johnfavour <favourjohn@gmail.com>
2023-08-23 16:20:42 +0100

    react-props checkpoint

--- src/Player.js
+++ src/Player.js
@@ -1,18 +1,18 @@
 import Card from 'react-bootstrap/Card';
 
 
-const Player = ({name, age, team, nationality, jerseyNumber, imageUrl}) => {
+const Player = ({Name, Age, Team, Nationality, JerseyNumber, ImageUrl}) => {
   return (
     // The card tag where the players data is displayed
     <Card style={{ width: '18rem', border: 'none' }}>
-      <Card.Img variant="top" src={imageUrl} />
+      <Card.Img variant="top" src={ImageUrl} />
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
+        <Card.Title>{Name}</Card.Title>
         <Card.Text>
-          team: {team} <br/>
-          age: {age} <br/>
-          nationality: {nationality} <br/>
-          jerseyNumber: {jerseyNumber} 
+          Team: {Team} <br/>
+          Age: {Age} <br/>
+          Nationality: {Nationality} <br/>
+          JerseyNumber: {JerseyNumber} 
         </Card.Text>
       </Card.Body>
     </Card>
--- src/players.js
+++ src/players.js
@@ -2,36 +2,36 @@
 // an array of JSON Objects containing the player’s details 
 const players = [
     {
-      name: "Lionel Messi",
-      age: 36,
-      team: "Inter Miami",
-      nationality: "Argentine",
-      jerseyNumber: "10",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj5p2s2LnnOXmoDWDyqMuf-FPr2Uc3yuqZoA&usqp=CAU"
+      Name: "Lionel Messi",
+      Age: 36,
+      Team: "Inter Miami",
+      Nationality: "Argentine",
+      JerseyNumber: "10",
+      ImageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj5p2s2LnnOXmoDWDyqMuf-FPr2Uc3yuqZoA&usqp=CAU"
     },
     {
-        name: "Cristiano Ronaldo",
-        age: 38,
-        team: "Al-Nassr",
-        nationality: "Portuguese",
-        jerseyNumber: "7",
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkAfpbP6iMG_RYnYyyPt3BcNXndQhY3cWZq_GuGumzPx0JNynMWtoc6X1AkaGrcvLqcQw&usqp=CAU"
+        Name: "Cristiano Ronaldo",
+        Age: 38,
+        Team: "Al-Nassr",
+        Nationality: "Portuguese",
+        JerseyNumber: "7",
+        ImageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkAfpbP6iMG_RYnYyyPt3BcNXndQhY3cWZq_GuGumzPx0JNynMWtoc6X1AkaGrcvLqcQw&usqp=CAU"
     },
     {
-        name: "Neymar",
-        age: 31,
-        team: "Al Hilal",
-        nationality: "Brazilian",
-        jerseyNumber: "10",
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_Nn5s5W8L985ykRwqOkER8IU8C38qy3q8xw&usqp=CAU"
+        Name: "Neymar",
+        Age: 31,
+        Team: "Al Hilal",
+        Nationality: "Brazilian",
+        JerseyNumber: "10",
+        ImageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_Nn5s5W8L985ykRwqOkER8IU8C38qy3q8xw&usqp=CAU"
     },
     {
-        name: "Kylian Mbappe",
-        age: 24,
-        team: "Paris Saint-German",
-        nationality: "France",
-        jerseyNumber: "10",
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQopoWZ7nBF9uUykgeG8u0cG4gbiQxJFKH0A&usqp=CAU"
+        Name: "Kylian Mbappe",
+        Age: 24,
+        Team: "Paris Saint-German",
+        Nationality: "France",
+        JerseyNumber: "10",
+        ImageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQopoWZ7nBF9uUykgeG8u0cG4gbiQxJFKH0A&usqp=CAU"
     }
   ];
   
